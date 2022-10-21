73b692d56f86028e4c2a1432fdb5aa3ce4382897
Hari Krishna Boyapati <harikrishnamsds@gmail.com>
2022-10-21 12:57:07 -0500

    updated school.sh

--- school.sh
+++ school.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cat Property_Tax_Roll.csv|grep "MADISON SCHOOLS"|cut -d, -f7| { count=0;sum=0;while read n; do count=$(($count+1));sum=$(($sum+$n));done;average=$(($sum/$count));echo $average; }
+cat Property_Tax_Roll.csv|
+grep "MADISON SCHOOLS"|
+cut -d, -f7|
+{ count=0;sum=0;while read n; do count=$(($count+1));sum=$(($sum+$n));done;average=$(($sum/$count));echo $average; }
