fa606826bab4a444a44968d414d07698158207ea
Harshita <edugudi@wisc.edu>
2022-10-21 09:40:22 -0500

    Updated file five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+mkdir ./five
+cd five
+for subdir in dir{1..5}
+do mkdir $subdir
+   cd $subdir
+   for number in {1..4}
+   do filename="file$number"
+      for val in $(seq 1 $number)
+      do echo $number >> $filename
+      done
+   done
+   cd ..
+done
+
