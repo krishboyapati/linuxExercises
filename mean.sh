e8eed9be23319b0ae926e9f19966f079b6e60edb
Harshita <edugudi@wisc.edu>
2022-10-21 10:09:33 -0500

    Updated mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+if [[ $# -gt 2 || $# -eq 0 ]]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 0
+fi
+
+if [[ $# -eq 2 ]]; then
+    data=$(tail -n +2 $2 | cut -d "," -f $1)
+else
+    read name
+    if [[ $name == *.csv ]]; then
+      data=$(tail -n +2 $name | cut -d "," -f $1)
+    else
+      data=$(tail -n +2 $2 | cut -d "," -f $1)
+    fi
+fi
+for sample in $data
+do
+    sum=$(($sum+$sample))
+    count=$(($count+1))
+done
+echo $(($sum/$count))
